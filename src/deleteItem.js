90d3691ac8a786adca0acb300ae9f8d1660b7630
che36 <tron36.cc@gmail.com>
2021-09-06 11:45:55 -0300

    final project with delete action added

---
+++ src/deleteItem.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const AWS = require("aws-sdk")
+
+const deleteItem = async (event) => {
+
+  const {itemStatus} = JSON.parse(event.body);
+  const {id} = event.pathParameters
+
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  await dynamodb.delete({
+    TableName: "ItemTable",
+    Key: {id},
+    UpdateExpression: 'set itemStatus = :itemStatus',
+    ExpressionAttributeValues: {
+      ':itemStatus': itemStatus
+    },
+    ReturnValues: "ALL_NEW"
+  }).promise()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      { msg: 'Item deleted'}
+    ),
+  };
+};
+
+
+module.exports = {
+    handler:deleteItem
+}
+
